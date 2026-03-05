c1518fdaa4bbb3b7d4716220135d8858d8dbe760
VULKAI007 <enzoalcantara67@gmail.com>
2026-03-05 14:44:31 -0300

    FEAT: switch-case em js

---
+++ 6.switch-case.js
@@ -0,0 +1,21 @@
+let opcao = 3
+
+switch (opcao) {
+    case 1:
+        console.log("OPÇÃO 1.")
+        break;
+    case 2:
+        console.log("OPÇÃO 2.")
+        break;
+    case 3:
+        console.log("OPÇÃO 3.")
+        break;
+    case 4:
+        console.log("OPÇÃO 4.")
+        break;
+    case 5:
+        console.log("OPÇÃO 5.")
+        break;
+    default:
+        break;
+}
