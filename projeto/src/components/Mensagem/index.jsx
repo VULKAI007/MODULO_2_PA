a5bd34db4e5178fa0dbd0d8ab50bf694e833d95a
Laiane <laiane.s.santos7@ba.estudante.senai.br>
2026-03-26 16:54:25 -0300

    Introdução ao react

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function Mensagem() {
+    return (
+        // COMANDOS HTML
+        <div className='mensagem'>
+            <h1>Componente</h1>
+            <p>Este é um componente com uma mensagem simples</p>
+        </div>
+    )
+    
+}
+
+export default Mensagem
